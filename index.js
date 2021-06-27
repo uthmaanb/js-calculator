f5377ac635b9f698c4131ff6d6ac8f8b5f2a5790
Uthmaan Breda <uthmaanbreda@gmail.com>
2021-06-27 23:55:54 +0200

    display fixed

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-
 // function that displays the numbers on the screen
-function click(val) {
-    document.getElementById("screen").value =
-      document.getElementById("screen").value + val;
-  }
-  
-  // function that clears everything on the screen
-  function clrdisp() {
-    document.getElementById("screen").value = "";
-  }
-  
-  // function that makes the equal button show the results
-  function equal() {
-    let text = document.getElementById("screen").value;
-    let result = eval(text);
-    document.getElementById("screen").value = result;
-  }
+function clk(val) {
+  document.getElementById("display").value =
+    document.getElementById("display").value + val;
+}
+
+// function that clears everything on the screen
+function clrdisp() {
+  document.getElementById("display").value = "";
+}
+
+// function that makes the equal button show the results
+function equal() {
+  let text = document.getElementById("display").value;
+  let result = eval(text);
+  document.getElementById("display").value = result;
+}
